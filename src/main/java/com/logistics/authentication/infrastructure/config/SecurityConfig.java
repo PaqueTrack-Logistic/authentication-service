1e8bf74b4a23304d12539911345f164bdad950c9
RebeBg <rebecabedoyagallego@gmail.com>
2026-06-02 14:08:07 -0500

    Actualización en configuración de seguridad y properties

--- src/main/java/com/logistics/authentication/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/logistics/authentication/infrastructure/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
 						.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 						.requestMatchers("/api/v1/auth/login", "/api/v1/auth/refresh").permitAll()
 						.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+						.requestMatchers("/actuator/**").permitAll()
 						.requestMatchers("/error").permitAll()
 						.requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
 						.anyRequest().authenticated())
